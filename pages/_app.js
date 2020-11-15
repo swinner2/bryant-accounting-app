042cf9d67931fb8f2086a54a08244543fc678f60
Sean Winner <seanwinner@Seans-MacBook-Pro-2.local>
2020-11-15 11:35:27 -0800

    add global state to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,40 @@
 import "../styles/tailwind.css";
+import { useEffect, useState } from "react";
+import netlifyAuth from "../netlifyAuth.js";
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  let [loggedIn, setLoggedIn] = useState(netlifyAuth.isAuthenticated);
+  let [user, setUser] = useState(null);
+
+  useEffect(() => {
+    netlifyAuth.initialize((user) => {
+      setLoggedIn(!!user);
+      setUser(user);
+      if (!user) {
+        console.log("not logged in");
+
+        login();
+      }
+    });
+  }, [loggedIn]);
+
+  let login = () => {
+    netlifyAuth.authenticate((user) => {
+      setLoggedIn(!!user);
+      setUser(user);
+      Router.reload("/");
+    });
+  };
+
+  let logout = () => {
+    netlifyAuth.signout(() => {
+      setLoggedIn(false);
+      setUser(null);
+    });
+    Router.reload("/");
+  };
+
+  return (
+    <Component {...pageProps} login={login} user={user} loggedIn={loggedIn} />
+  );
 }
--- pages/filing.js
+++ pages/filing.js
@@ -1,42 +1,8 @@
 import Head from "next/head";
-import Router from "next/router";
 import Dashboard from "@components/Dashboard";
-import Settings from "@components/Settings";
 import { useEffect, useState } from "react";
-import netlifyAuth from "../netlifyAuth.js";
 
-export default function Filings() {
-  let [loggedIn, setLoggedIn] = useState(netlifyAuth.isAuthenticated);
-  let [user, setUser] = useState(null);
-
-  useEffect(() => {
-    netlifyAuth.initialize((user) => {
-      setLoggedIn(!!user);
-      setUser(user);
-      if (!user) {
-        console.log("not logged in");
-
-        login();
-      }
-    });
-  }, [loggedIn]);
-
-  let login = () => {
-    netlifyAuth.authenticate((user) => {
-      setLoggedIn(!!user);
-      setUser(user);
-      Router.reload("/");
-    });
-  };
-
-  let logout = () => {
-    netlifyAuth.signout(() => {
-      setLoggedIn(false);
-      setUser(null);
-    });
-    Router.reload("/");
-  };
-
+export default function Filings({ loggedIn, user, logout }) {
   return (
     <div className="container">
       <Head>
